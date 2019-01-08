9aa6d09de9193347309e0ccdbc49a55088b119ab
user-of-git <username696969696969696969@gmail.com>
2019-01-08 14:57:58 -0500

    staticfile

--- planner/settings.py
+++ planner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'm&xdy0ni7g@llj1wd%e3wz3q&ulsry
 # dict value set as '' to evaluate as False by bool()
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
 
-ALLOWED_HOSTS = ['matts-task-planner.herokuapp.com']
+ALLOWED_HOSTS = ['matts-task-planner.herokuapp.com', "127.0.0.1"]
 
 # Application definition
 
@@ -119,8 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
