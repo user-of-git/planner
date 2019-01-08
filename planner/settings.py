7e3553d137821103998cb9e9ecb3c42848b2f171
user-of-git <username696969696969696969@gmail.com>
2019-01-08 14:15:25 -0500

    undo heroku static settings

--- planner/settings.py
+++ planner/settings.py
@@ -124,11 +124,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = 'introduction'
 
