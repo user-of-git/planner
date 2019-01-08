766744c9e61e36d208522a9b6e43995d405211e5
user-of-git <44252448+user-of-git@users.noreply.github.com>
2019-01-08 12:24:47 -0500

    add staticfiles_storage = whitenoise

--- planner/settings.py
+++ planner/settings.py
@@ -142,3 +142,7 @@ ADMINS = [('name', 'name@example.com')]
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
